6b372eb56b8a656910365fe1e43f2cd819101c77
DharmarayN <145792790+DharmarayN@users.noreply.github.com>
2023-09-22 22:17:15 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.title('my Parents New Healthy Diner')
 streamlit.header('Break Past')
-streamlit.text('Uppittu', 'Avalakki', 'Idli')
+streamlit.text('Uppittu, Avalakki, Idli')
