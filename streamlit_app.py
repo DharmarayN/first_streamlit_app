8e05f48ca375af8f647fe8a43027d37b6bd19e74
DharmarayN <145792790+DharmarayN@users.noreply.github.com>
2023-09-22 22:27:37 +0530

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 streamlit.title('my Parents New Healthy Diner')
 streamlit.header('Break Past')
 streamlit.text('Uppittu, Avalakki, Idli')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
